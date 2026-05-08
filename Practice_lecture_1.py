b238f7e9e0511d98b0d8bd97095ccc9a7c2d74df
Yashhx07 <yashbakle250@gmail.com>
2026-05-08 16:01:51 +0530

    Add files via upload

---
+++ Practice_lecture_1.py
@@ -0,0 +1,36 @@
+#1 get input 2 numbers anf print the sum
+
+# a = int(input("Enter any number : "))
+
+# b = int(input("Enter any number : "))
+
+# sum = a+b
+
+# print(sum)
+
+
+
+#2 Get length of squre and print its area
+
+# l = int(input("Enter length of squre : "))
+
+# area = l**2
+
+# print(area)
+
+#3 Get 2 floating values as input and print their avg
+
+# a = float(input("Enter a : "))
+# b = float(input("Enter a : "))
+
+# avg = (a+b)/2
+
+# print(avg)
+
+#4 Get 2 int numbers as input if a is greater than or equal to
+# b then print true otherwise print false
+
+# a = int(input("Enter number : "))
+# b = int(input("Enter number : "))
+
+# print(a>=b)
